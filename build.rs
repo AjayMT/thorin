3e7a02ebb0f832ac8a191ede1987502d78c5c9b3
Ajay MT <ajay.tatachar@gmail.com>
2019-04-03 19:45:08 -0500

    don't build with mach files on linux

--- build.rs
+++ build.rs
@@ -2,11 +2,21 @@
 extern crate cc;
 
 
+#[cfg(target_os = "macos")]
 fn main() {
     cc::Build::new()
         .file("src/thorin.c")
         .file("src/mig/mach_excServer.c")
         .file("src/mig/mach_excUser.c")
+        .flag("-Wno-unused-parameter")
+        .flag("-Wno-unused-function")
+        .compile("thorin");
+}
+
+#[cfg(target_os = "linux")]
+fn main() {
+    cc::Build::new()
+        .file("src/thorin.c")
         .flag("-Wno-unused-parameter")
         .flag("-Wno-unused-function")
         .compile("thorin");
